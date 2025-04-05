fabc63ca5e5bf3abc9dbd2a4930b4f939d865bab
Y-Sagar-developer <sagaryarra987@gmail.com>
2025-04-05 11:01:17 +0530

    change title name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
             <div className="footer__content container">
                 <div className="footer__content__logo">
                     <div className="logo">
-                        <img src={logo} alt="" />
-                        <Link to="/">tMovies</Link>
+                        {/* <img src={logo} alt="" /> */}
+                        <Link to="/">My Movies</Link>
                     </div>
                 </div>
                 <div className="footer__content__menus">
